ce147803f1f192813771823f252d86be8f4ac8cd
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-04-07 08:32:59 -0300

    First project in react

---
+++ react/react-for-beginners/TestApp1/src/App.tsx
@@ -0,0 +1,7 @@
+import Message from './Message';
+
+function App() {
+  return <div><Message></Message></div>;
+  //return <div> <Message /> </div>; // another option is to use this way    
+ }
+ export default App;
---
+++ react/react-for-beginners/TestApp1/src/Message.tsx
@@ -0,0 +1,6 @@
+function Message() {
+    // This is JSX: Js xml
+    return <h1> Hello World </h1>;
+ }
+
+ export default Message;
