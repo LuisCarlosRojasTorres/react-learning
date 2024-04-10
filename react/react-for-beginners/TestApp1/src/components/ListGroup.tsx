927690e6e4f80c164a4179931a38cbc5e211ebd1
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-04-10 01:59:51 -0300

    Conditional render

--- react/react-for-beginners/TestApp1/src/components/ListGroup.tsx
+++ react/react-for-beginners/TestApp1/src/components/ListGroup.tsx
@@ -1,16 +1,17 @@
 function ListGroup() {
+  let items = ["city1", "city2", "city3", "city4"];
+  items = [];
+
   return (
     <>
       <h1> Dummy heading </h1>
+      {items.length === 0 ? <p>No items found!!!</p> : null}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
 }
-
 export default ListGroup;
