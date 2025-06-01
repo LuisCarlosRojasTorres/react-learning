242f4ebbe31ce98798c238383097a4144043dbec
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2025-06-01 20:32:34 -0300

    Deleted previous files

--- react/react-for-beginners/TestApp1/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import ListGroup from "./components/ListGroup";
-
-function App() {
-  return <div><ListGroup/></div>;
-  //return <div> <Message /> </div>; // another option is to use this way    
- }
- export default App;
